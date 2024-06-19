d3786ef6da3b936228f337d627d5aa4ae4ed0888
luxeivan <luxeivan@gmail.com>
2024-06-19 12:32:33 +0300

    подсвечивание поля во время ошибки

--- src/components/InputJson.js
+++ src/components/InputJson.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Input, Typography } from 'antd';
+import { Button, Input, Typography } from 'antd';
 import useStore from '../store/useStore';
 
 const { TextArea } = Input;
@@ -7,26 +7,42 @@ const { TextArea } = Input;
 export default function InputJson() {
     const json = useStore(state => state.json)
     const createJson = useStore(state => state.createJson)
-    const [value,setValue] = useState(false)
+    const clearJson = useStore(state => state.clearJson)
+    const err = useStore(state => state.err)
+    const [value, setValue] = useState(false)
     useEffect(() => {
-        try {            
-            console.log(localStorage.getItem('json'))
-            createJson(localStorage.getItem('json'))
-            setValue(localStorage.getItem('json'))
+        try {
+            // console.log(localStorage.getItem('json'))
+            if (localStorage.getItem('json')) {
+                createJson(localStorage.getItem('json'))
+                // setValue(localStorage.getItem('json'))
+            } else {
+                setValue(JSON.stringify(json, undefined, 4))
+            }
         } catch (error) {
-            
+
         }
     }, [])
+    useEffect(() => {
+        try {
+            setValue(JSON.stringify(json, undefined, 4))
+        } catch (error) {
 
+        }
+    }, [json])
+    const clear = () => {
+        localStorage.removeItem('json')
+        clearJson()
+    }
     const handlerChange = (event) => {
-        console.log(event.target.value)
+        // console.log(event.target.value)
         createJson(event.target.value)
         setValue(event.target.value)
     }
     return (
         <div>
-            <Typography.Title level={3} style={{ textAlign: "center" }}>Ввод JSON для формы</Typography.Title>
-            <TextArea rows={4} autoSize={{ minRows: 6 }} onChange={handlerChange} value={value}/>
+            <Typography.Title level={3} style={{ textAlign: "center" }}>Ввод JSON для формы <Button onClick={clear}>Сбросить</Button></Typography.Title>
+            <TextArea rows={4} autoSize={{ minRows: 6 }} onChange={handlerChange} value={value} style={err && { border: "1px solid red", backgroundColor: "rgba(255,0,0,0.1)" }} />
         </div>
     )
 }
--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,25 +1,24 @@
 import { create } from 'zustand'
+import formJson from '../components/Form.json'
 
 const useStore = create((set, get) => ({
-    json: [
-        {
-            "type": "divider",
-            "label": "ФИО"
-        },
-        {
-            "type": "textInput",
-            "name": "Фамилия",
-            "label": "Фамилия",
-            "placeholder": "Иванов",
-            "required": true
-        },
-    ],
+    json: formJson,
+    err: false,
     createJson: (value) => {
         try {
-            set({ json: JSON.parse(value) })
+            set({ json: JSON.parse(value), err: false })
             localStorage.setItem('json', value)
         } catch (error) {
             console.log(error)
+            set({ err: true })
+        }
+    },
+    clearJson: () => {
+        try {
+            set({ json: formJson, err: false })
+        } catch (error) {
+            console.log(error)
+            set({ err: true })
         }
     }
 
